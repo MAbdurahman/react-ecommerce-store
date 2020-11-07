bb6b7ee13680a565e08a724fb08be1debea47379
MAbdurahman <mdbdrrhm@gmail.com>
2020-11-07 08:36:41 -0600

    Add react-router-dom and improve zoom effect on MenuItem

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -4,7 +4,7 @@ import './MenuItem.scss';
 const MenuItem = ({ title, imageUrl, size }) => {
    return (
       <div className={`${size} menu-item`}>
-         <div className="background-image" style={{
+         <div className="background-image zoom" style={{
             backgroundImage: `url(${imageUrl})`
          }} />
          <div className="content">
