bb45ba967412225116afb7628e1298d5fd81a3c0
MAbdurahman <mdbdrrhm@gmail.com>
2020-11-09 09:58:58 -0600

    Update firebase-utils.js and CustomButton.scss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/home-page/HomePage';
 import ShopPage from './pages/shop-page/ShopPage';
 import Header from './components/header/Header';
 import SignInUpPage from './pages/sign-in-up-page/SignInUpPage';
-import { auth } from './firebase/firebase-utils';
+import { auth, createUserProfileDocument } from './firebase/firebase-utils';
 
 import './App.css';
 
@@ -20,11 +20,20 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-	this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-			this.setState({
-				currentUser: user
-			});
-			console.log(user);
+	this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+				userRef.onSnapshot(snapShot => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data
+
+						}
+					});
+				});
+			}
+			this.setState({currentUser: userAuth});
 		});
 	};
 
